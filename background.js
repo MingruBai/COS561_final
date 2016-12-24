5ba37eb118377be245c54c3c182fc95fc6e510e2
Victor Bai <mingru.bai@princeton.edu>
2016-12-24 15:56:05 -0500

    notification working

--- background.js
+++ background.js
@@ -7,10 +7,13 @@ function processURL(requestDetails) {
 	}
 	
 	console.log("Processing: " + url.hostname);
+	loadDubious()
 	// Issue traceRoute command to retrieve a list of IPs:
 	var ips = traceRoute(url.hostname);	
 	// Use ipinfo API to retrieve details for each IP:
 	translate(ips);
+	
+
 }
 
 // TODO: implement traceRoute command:
@@ -20,7 +23,6 @@ function traceRoute(hostname) {
 	return ips;
 }
 
-
 function translationCallback(response) {
 	if (response['loc'] == undefined) return;
 	if (response['org'] == undefined) return;
@@ -28,10 +30,32 @@ function translationCallback(response) {
 	browser.storage.local.set({
     	ipDetails: ipDetails
   	});
+  	
+  	// alert if dubious asn:
+  	var asString = response["org"].split(" ")[0];
+  	var asnString = asString.substring(2);  	
+  	if (dubiousAsn.indexOf(asnString) > -1) {
+  		browser.notifications.create({
+	    	"type": "basic",
+			"title": " Dubious AS Alert!",
+			"message":  "Web content delivered through dubious AS " + asnString
+		});
+  	}
+  	
+  	// alert if dubious geo:
+  	var countryString = response["country"];
+  	if (dubiousGeo.indexOf(countryString) > -1) {
+  		browser.notifications.create({
+	    	"type": "basic",
+			"title": "Dubious Geo Alert!",
+			"message":  "Web content delivered through dubious geo " + countryString
+		});
+  	}
 }
 
 // Completed: use ipinfo.io to retrieve IP details:
 function translate(ips) {
+	ipDetails = [];
 	for (var i = 0; i < ips.length; i++) {
 		var ip = ips[i];
 		var myRequest = new Request("http://ipinfo.io/" + ip + "/json");	
@@ -43,7 +67,23 @@ function translate(ips) {
 	}
 }
 
+function loadDubious() {
+  var gettingItem = browser.storage.local.get('geo');
+  gettingItem.then((res) => {
+    var dubiousGeoString = res.geo || 'North Korea, Russia';
+    dubiousGeo = dubiousGeoString.split(", ");
+  });
+  
+  gettingItem = browser.storage.local.get('asn');
+  gettingItem.then((res) => {
+    var dubiousAsnString = res.asn || '1000, 1101, 1200';
+    dubiousAsn = dubiousAsnString.split(", ");
+  });
+}
+
 var ipDetails = [];
+var dubiousGeo = [];
+var dubiousAsn = [];
 // always use full address (with www.)
 browser.webRequest.onBeforeRequest.addListener(
 	processURL,
