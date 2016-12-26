a0e0779a6920213312e2abc43391e022b56f5bd1
Victor Bai <mingru.bai@princeton.edu>
2016-12-25 19:05:29 -0500

    route change notification implemented

--- background.js
+++ background.js
@@ -7,19 +7,18 @@ function processURL(requestDetails) {
 	}
 	
 	console.log("Processing: " + url.hostname);
-	loadDubious()
+	loadDubious();
 	// Issue traceRoute command to retrieve a list of IPs:
 	var ips = traceRoute(url.hostname);	
 	// Use ipinfo API to retrieve details for each IP:
 	translate(ips);
-	
-
+	compareHistory(url, ips);
 }
 
 // TODO: implement traceRoute command:
 function traceRoute(hostname) {
 // 	var ips = ["192.168.0.1", "68.173.207.89", "68.173.198.16", "107.14.19.24", "66.109.6.27", "66.109.1.59", "209.51.175.37", "216.66.49.74", "128.112.12.130", "140.180.223.42"];
-	var ips = ["68.173.207.89", "140.180.223.42"];
+	var ips = ["140.180.223.42", "209.51.175.37"];
 	return ips;
 }
 
@@ -38,7 +37,7 @@ function translationCallback(response) {
   		browser.notifications.create({
 	    	"type": "basic",
 			"title": " Dubious AS Alert!",
-			"message":  "Web content delivered through dubious AS " + asnString
+			"message":  "Web content delivered through dubious AS " + asnString + "."
 		});
   	}
   	
@@ -48,7 +47,7 @@ function translationCallback(response) {
   		browser.notifications.create({
 	    	"type": "basic",
 			"title": "Dubious Geo Alert!",
-			"message":  "Web content delivered through dubious geo " + countryString
+			"message":  "Web content delivered through dubious geo " + countryString + "."
 		});
   	}
 }
@@ -79,6 +78,43 @@ function loadDubious() {
     var dubiousAsnString = res.asn || '1000, 1101, 1200';
     dubiousAsn = dubiousAsnString.split(", ");
   });
+}
+
+function compareHistory(url, ips) {
+	console.log(ips);
+	// Load existent stats with the storage API.
+	var gettingStoredRoute = browser.storage.local.get("hostNavigationRoute");
+	gettingStoredRoute.then(results => {
+  		// Initialize the saved stats if not yet initialized.
+  		if (!results.hostNavigationRoute) {
+    		results = {
+      		hostNavigationRoute: {}
+    		};
+  		}
+  		const {hostNavigationRoute} = results;
+  		var flag = true;
+  		if (hostNavigationRoute[url.hostname]) {
+  			console.log("History loaded: ");
+  			console.log(hostNavigationRoute[url.hostname]);
+			if (hostNavigationRoute[url.hostname].length != ips.length){
+				flag = false;
+			} else {
+				for (var i = 0; i < ips.length; i++) {
+					if (ips[i] != hostNavigationRoute[url.hostname][i]) flag = false;
+				}
+			}
+  		} 
+		hostNavigationRoute[url.hostname] = ips;
+		browser.storage.local.set(results);
+		
+		if (!flag) {
+		  	browser.notifications.create({
+	    	"type": "basic",
+			"title": "Routing Path Change Alert!",
+			"message":  "Routing path changed for " + url.hostname + "."
+			});
+		}
+  	});
 }
 
 var ipDetails = [];
